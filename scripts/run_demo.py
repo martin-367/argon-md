a91c14b46192c688146bf88d15be965915095aca
martin-367 <martindean367@gmail.com>
2025-08-24 22:33:15 +1000

    Update run_demo.py
    setup basic step integration utilising src/md and basic plot with matplotlib

--- scripts/run_demo.py
+++ scripts/run_demo.py
@@ -1,1 +1,25 @@
+from src import md
+import matplotlib.pyplot as plt
 
+positions = md.initialize_positions(N, L)
+velocities = md.initialize_velocities(N)
+forces = md.compute_forces(positions, L)
+
+energy_log = []
+temp_log = []
+
+for step in range(md.steps):
+  positions, velocities, forces = md.velocity_verlet(positions, velocities, forces, dt, L)
+  energy_log.append(md.compute_kinetic_energy(velocities))
+  temp_log.append(md.compute_temperature(velocities))
+
+# Plotting
+
+plt.plot(energy_log)
+plt.plot(temp_log)
+plt.show()
+
+# periodic boundary conditions
+# implement neighbour lists
+# introduce thermostats (berendsen or langevin
+# export trajectories for visualization in VMD or OVITO
